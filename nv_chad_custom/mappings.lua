2858b385047ffd3482d1abe59c2960162958f0ea
Steven McFarlane <steven@scientist.com>
2023-05-19 17:09:54 -0600

    nvchad updates

--- nv_chad_custom/mappings.lua
+++ nv_chad_custom/mappings.lua
@@ -1,12 +1,35 @@
 local M = {}
 
 -- In order to disable a default keymap, use
-M.disabled = {}
+M.disabled = {
+  i = {
+    -- navigate within insert mode
+    ["<C-h>"] = "",
+    ["<C-l>"] = "",
+    ["<C-j>"] = "",
+    ["<C-k>"] = "",
+  },
+
+  n = {
+    -- switch between windows
+    ["<C-h>"] = "",
+    ["<C-l>"] = "",
+    ["<C-j>"] = "",
+    ["<C-k>"] = "",
+  }
+}
 
 -- Your custom mappings
 M.abc = {
   n = {
-     ["<C-p>"] = {":Telescope Files <CR>", "Telescope Files"},
+    ["<C-p>"] = { ":Telescope find_files<CR>", "Telescope Files" },
+    -- tmux nav
+    ["<C-h"] = { ":NvimTmuxNavigateLeft<CR>", "Nvim Tmux Navigate Left" },
+    ["<C-j"] = { ":NvimTmuxNavigateDown<CR>", "Nvim Tmux Navigate Down" },
+    ["<C-k"] = { ":NvimTmuxNavigateUp<CR>", "Nvim Tmux Navigate Up" },
+    ["<C-l"] = { ":NvimTmuxNavigateRight<CR>", "Nvim Tmux Navigate Right" },
+    ["<C-["] = { ":NvimTmuxNavigateLastActive<CR>", "Nvim Tmux Navigate Last Active" },
+    ["<C-]"] = { ":NvimTmuxNavigateNext<CR>", "Nvim Tmux Navigate Next" },
   },
 
   i = {
--- nv_chad_custom/plugins.lua
+++ nv_chad_custom/plugins.lua
@@ -26,7 +26,26 @@ local plugins = {
         "solargraph"
       },
     },
-  }
+  },
+  -- custom plugins
+  { "tpope/vim-repeat", lazy = false },
+  { "tpope/vim-sensible", lazy = false },
+  { "tpope/vim-surround", lazy = false },
+  { "machakann/vim-highlightedyank", lazy = false },
+  { "ekalinin/Dockerfile.vim" },
+  { "ThePrimeagen/harpoon" },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+      nvim_tmux_nav.setup {
+        disable_when_zoomed = true -- defaults to false
+      }
+    end,
+    lazy = false
+  },
+  { "github/copilot.vim", lazy = false }
 }
 
 return plugins
