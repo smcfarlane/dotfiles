9fbc497dcc8061b872b6e500e7b04e33d93dd6b2
Steven McFarlane <steven@scientist.com>
2023-07-27 08:20:41 -0600

    try to fix indent issues

--- nv_chad_custom/chadrc.lua
+++ nv_chad_custom/chadrc.lua
@@ -1,9 +1,19 @@
 ---@type ChadrcConfig 
 local M = {}
 M.ui = {
-  theme_toggle = { "gruvbox", "gruvbox_light" },
-  theme = 'gruvbox'
+  theme_toggle = { "everblush", "everblush_light" },
+  theme = 'everblush'
 }
 M.mappings = require "custom.mappings"
 M.plugins = "custom.plugins"
+
+
+local opt = vim.opt
+local g = vim.g
+
+opt.smartindent = true
+opt.relativenumber = true
+opt.breakindent = true
+opt.showbreak = "  ↳"
+
 return M
